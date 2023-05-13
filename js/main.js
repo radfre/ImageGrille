9ba412086bf5749280e759b051b073f5dc8188fd
radfre <schatz.alex@gmail.com>
2023-05-13 16:32:33 -0500

    gllery test

--- js/main.js
+++ js/main.js
@@ -171,4 +171,23 @@ jQuery(document).ready(function ($) {
   });
 
 
+  // porfolio carousel (uses the Owl Carousel library)
+  $(".clients-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      768: {
+        items: 4
+      },
+      900: {
+        items: 6
+      }
+    }
+  });
+
+
 });
